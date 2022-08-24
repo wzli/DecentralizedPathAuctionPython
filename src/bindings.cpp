20d0f40f68b9551fac488c566a16f2fbdbf3ec7f
WenZheng Li <me@wenzheng.li>
2022-08-24 23:29:52 +0900

    working graph bindings

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -8,34 +8,72 @@ namespace py = pybind11;
 using namespace pybind11::literals;
 using namespace decentralized_path_auction;
 
-PYBIND11_MAKE_OPAQUE(std::vector<Visit>);
-PYBIND11_MAKE_OPAQUE(std::vector<std::shared_ptr<Node>>);
+PYBIND11_MAKE_OPAQUE(Nodes);
+PYBIND11_MAKE_OPAQUE(Path);
 
 PYBIND11_MODULE(bindings, dpa) {
-    // graph.hpp
-    py::class_<Point>(dpa, "Point")
-        .def(py::init<float const &, float const &, float const &>())
-        .def_property("x", &Point::get<0>, &Point::set<0>)
-        .def_property("y", &Point::get<1>, &Point::set<1>)
-        .def_property("z", &Point::get<2>, &Point::set<2>)
-    ;
+    py::bind_vector<Nodes>(dpa, "Nodes");
+    py::bind_vector<Path>(dpa, "Path");
 
-    py::class_<Node> node(dpa, "Node");
+    // Point
+    py::class_<Point> point(dpa, "Point");
+    point.def(py::init<float const &, float const &, float const &>());
+    point.def_property("x", &Point::get<0>, &Point::set<0>);
+    point.def_property("y", &Point::get<1>, &Point::set<1>);
+    point.def_property("z", &Point::get<2>, &Point::set<2>);
+    point.def("tup",
+        [](const Point &p) {
+            return std::make_tuple(p.get<0>(), p.get<1>(), p.get<2>());
+        }
+    );
 
-    py::enum_<Node::State>(node, "State")
-        .value("DEFAULT", Node::DEFAULT)
-        .value("NO_PARKING", Node::NO_PARKING)
-        .value("NO_STOPPING", Node::NO_STOPPING)
-        .value("DISABLED", Node::DISABLED)
-        .value("DELETED", Node::DELETED)
-        .export_values();
+    // Node
+    py::class_<Node, NodePtr> node(dpa, "Node");
+    node.def(py::init<Point>());
+    node.def_readonly("position", &Node::position);
+    node.def_readwrite("state", &Node::state);
+    node.def_readwrite("edges", &Node::edges);
+    // node.def_readwrite("auction", &Node::auction);
+    node.def_readwrite("custom_data", &Node::custom_data);
+    // node.def("validate", &Node::validate);
 
-    node
-        .def(py::init<Point>())
-        .def_readonly("position", &Node::position)
-        .def_readwrite("state", &Node::state)
-        .def_readwrite("edges", &Node::edges)
-        // .def_readwrite("auction", &Node::auction)
-        .def_readwrite("custom_data", &Node::custom_data)
-    ;
+    // Node::State
+    py::enum_<Node::State> state(node, "State");
+    state.value("DEFAULT", Node::DEFAULT);
+    state.value("NO_PARKING", Node::NO_PARKING);
+    state.value("NO_STOPPING", Node::NO_STOPPING);
+    state.value("DISABLED", Node::DISABLED);
+    state.value("DELETED", Node::DELETED);
+    state.export_values();
+
+    // Visit
+    py::class_<Visit> visit(dpa, "Visit");
+    visit.def(py::init<NodePtr>());
+    visit.def_readwrite("price", &Visit::price);
+    visit.def_readwrite("duration", &Visit::duration);
+    visit.def_readwrite("base_price", &Visit::base_price);
+    visit.def_readwrite("cost_estimate", &Visit::cost_estimate);
+    visit.def_readwrite("time_estimate", &Visit::time_estimate);
+
+    // NodeRTree
+    py::class_<NodeRTree> node_rtree(dpa, "NodeRTree");
+    node_rtree.def(py::init<>());
+    node_rtree.def("insertNode", &NodeRTree::insertNode, "node"_a);
+    node_rtree.def("removeNode", &NodeRTree::removeNode, "node"_a);
+    node_rtree.def("clearNodes", &NodeRTree::clearNodes);
+
+    node_rtree.def("findNode", &NodeRTree::findNode, "position"_a);
+    node_rtree.def("findNearestNode", &NodeRTree::findNearestNode, "position"_a, "criterion"_a);
+    node_rtree.def("findAnyNode", &NodeRTree::findAnyNode, "criterion"_a);
+
+    node_rtree.def("containsNode", &NodeRTree::containsNode, "node"_a);
+
+    // Graph
+    py::class_<Graph, NodeRTree> graph(dpa, "Graph");
+    graph.def(py::init<>());
+    graph.def("clearNodes", &Graph::clearNodes);
+    graph.def("removeNode", static_cast<bool (Graph::*)(NodePtr)>(&Graph::removeNode), "node"_a);
+    graph.def("removeNode", static_cast<bool (Graph::*)(Point)>(&Graph::removeNode), "node"_a);
+    graph.def("insertNode", &Graph::insertNode<>, "position"_a);
+
 }
--- test/test_bindings.py
+++ test/test_bindings.py
@@ -1,5 +1,7 @@
 from decentralized_path_auction_python import *
 
+import sys
+
 def test_point():
     p = Point(1,2,3)
     assert(p.x == 1)
@@ -11,3 +13,31 @@ def test_point():
     assert(p.x == 4)
     assert(p.y == 5)
     assert(p.z == 6)
+
+def test_graph():
+    graph = Graph()
+    pathway = [graph.insertNode(Point(0,0,0))]
+    for i in range(1, 10):
+        pos = Point(i, 0,0)
+        node = graph.insertNode(pos)
+        assert(node)
+        pathway[-1].edges.append(node)
+        node.edges.append(pathway[-1])
+        pathway.append(node)
+
+    for node in pathway[1: -1]:
+        assert(graph.containsNode(node))
+        assert(len(node.edges) == 2)
+
+    for i in range(0, 10):
+        pos = Point(i, 0,0)
+        assert(graph.findNode(pos).position.tup() == pos.tup())
+
+        pos_offset = Point(i, 10, 0);
+        assert(graph.findNearestNode(pos_offset, Node.State.DEFAULT).position.tup() == pos.tup())
+
+        assert(graph.removeNode(pos))
+        assert(not graph.findNode(pos))
+
+    #print("", file=sys.stderr)
+    #print(node.position.tup(), node.state, file=sys.stderr)
