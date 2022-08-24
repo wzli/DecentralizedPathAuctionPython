3c0dc1dcaaf7db6b625488e1f7d52c0a4b435e8a
WenZheng Li <me@wenzheng.li>
2022-08-24 20:26:24 +0900

    wip

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,17 +1,41 @@
 #include <decentralized_path_auction/graph.hpp>
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
+
 namespace py = pybind11;
-namespace dpa = decentralized_path_auction;
+using namespace pybind11::literals;
+using namespace decentralized_path_auction;
 
-
-// graph.hpp
+PYBIND11_MAKE_OPAQUE(std::vector<Visit>);
+PYBIND11_MAKE_OPAQUE(std::vector<std::shared_ptr<Node>>);
 
 PYBIND11_MODULE(bindings, dpa) {
-    py::class_<dpa::Point>(dpa, "Point")
+    // graph.hpp
+    py::class_<Point>(dpa, "Point")
         .def(py::init<float const &, float const &, float const &>())
-        .def_property("x", &dpa::Point::get<0>, &dpa::Point::set<0>)
-        .def_property("y", &dpa::Point::get<1>, &dpa::Point::set<1>)
-        .def_property("z", &dpa::Point::get<2>, &dpa::Point::set<2>)
+        .def_property("x", &Point::get<0>, &Point::set<0>)
+        .def_property("y", &Point::get<1>, &Point::set<1>)
+        .def_property("z", &Point::get<2>, &Point::set<2>)
+    ;
+
+    py::class_<Node> node(dpa, "Node");
+
+    py::enum_<Node::State>(node, "State")
+        .value("DEFAULT", Node::DEFAULT)
+        .value("NO_PARKING", Node::NO_PARKING)
+        .value("NO_STOPPING", Node::NO_STOPPING)
+        .value("DISABLED", Node::DISABLED)
+        .value("DELETED", Node::DELETED)
+        .export_values();
+
+    node
+        .def(py::init<Point>())
+        .def_readonly("position", &Node::position)
+        .def_readwrite("state", &Node::state)
+        .def_readwrite("edges", &Node::edges)
+        // .def_readwrite("auction", &Node::auction)
+        .def_readwrite("custom_data", &Node::custom_data)
     ;
 }
